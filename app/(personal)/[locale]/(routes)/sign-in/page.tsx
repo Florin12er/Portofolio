7c9cb78aa90d3aad17f9d770c63a47abe37c73fc
Florin12er <apetreiflorin236@gmail.com>
2025-02-22 12:30:22 +0100

    added authencation pages

---
+++ app/(personal)/[locale]/(routes)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import SignIn from "./sign-in";
+export default function Register() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 py-20">
+      <SignIn />
+    </div>
+  );
+}
---
+++ app/(personal)/[locale]/(routes)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { SignUp } from "./sign-up";
+export default function Register() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 py-20">
+      <SignUp />
+    </div>
+  );
+}
